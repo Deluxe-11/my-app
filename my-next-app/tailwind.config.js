8788700bcfc50f9d8d8910c6f3e8915957a9db2b
tuannguyensn2001 <devpro2001@gmail.com>
2021-09-19 17:42:55 +0700

    setup next

---
+++ my-next-app/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // purge: [],
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/feature/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+
+}
